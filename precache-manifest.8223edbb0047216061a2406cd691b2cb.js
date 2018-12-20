7a956c7a9a0c50f2049e0a742de976fba40f903f
marimeli <marimel.casas@gmail.com>
2018-12-20 04:20:34 -0500

    Updates

---
+++ precache-manifest.8223edbb0047216061a2406cd691b2cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da547582403c644b188e",
+    "url": "/song-lyrics/static/js/runtime~main.da547582.js"
+  },
+  {
+    "revision": "2ffc702a4383a8b0420d",
+    "url": "/song-lyrics/static/js/main.2ffc702a.chunk.js"
+  },
+  {
+    "revision": "4d14a80511cbca376268",
+    "url": "/song-lyrics/static/js/1.4d14a805.chunk.js"
+  },
+  {
+    "revision": "2ffc702a4383a8b0420d",
+    "url": "/song-lyrics/static/css/main.6b87c6f3.chunk.css"
+  },
+  {
+    "revision": "54191c91c093794334e5ce19e5c0dc4a",
+    "url": "/song-lyrics/index.html"
+  }
+];
